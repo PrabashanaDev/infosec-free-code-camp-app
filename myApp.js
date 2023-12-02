1f57423e9bd20edc04fa4b50ad640c2c9189aa74
Chathura Prabashana <prabashana,dev@gmail.com>
2023-12-02 19:50:10 +0530

    Update errors and port

--- myApp.js
+++ myApp.js
@@ -2,11 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`Useful Programmer Info Security App Started on Port ${PORT}`);
-});
+
 
 
 
@@ -60,5 +57,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Useful Programmer Info Security App Started on Port ${port}`);
 });
